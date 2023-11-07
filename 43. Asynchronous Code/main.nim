dadb205747e4e8cff45139cf5bafb9480eabbfd6
WeebNetsu <stephenvdw7777@gmail.com>
2023-11-07 12:09:05 +0200

    Updated lesson 43 to use custom api endpoint

--- 43. Asynchronous Code/main.nim
+++ 43. Asynchronous Code/main.nim
@@ -1,48 +1,54 @@
-import std/[asyncdispatch, httpclient, json]
+import std/[asyncdispatch, httpclient, json, options]
 
 # compile: nim c -r -d:ssl main.nim
 
 type
-    StatusObj = object
-        verified: bool
-        sentCount: int
+    QuoteAuthorObj = object
+        name: string
+        language: string
+        ageOfDeath: Option[int]
+        dateOfBirth: string
 
-    FactObj = object
-        # _id: string
-        # _v: int
-        user: string
-        text: string
-        updatedAt: string
-        deleted: bool
-        source: string
+    QuoteUserObj = object
+        username: Option[string]
+        verified: bool
         `type`: string
-        status: StatusObj
+
+    QuoteObj = object
+        # NOTE save WITHOUT formatting, since the below format
+        # is VERY NB, since _ is not allowed as an object key
+        # so we need to use both ` and " to allow it
+        # `"_id"`: string
+        `"_id"`: string
+        quote: string
+        author: QuoteAuthorObj
+        user: QuoteUserObj
 
 # to create an async function, we can use {.async.}, which will allow us
 # to use await. To specify the data returned, we use Future[Data]
 # (future, since the data will be returned in the future after a request)
-proc fetchCatFacts(): Future[seq[FactObj]] {.async.} =
+proc fetchQuotes(): Future[seq[QuoteObj]] {.async.} =
     # we can create a new http client, in this case an async one, since
     # we want to be able to await it
     var client = newAsyncHttpClient()
     # we make a request to this API endpoint
-    # (more info https://alexwohlbruck.github.io/cat-facts/docs/)
-    var response = await client.get("https://cat-fact.herokuapp.com/facts")
+    # (more info https://github.com/WeebNetsu/ytapi.stevesteacher.com)
+    var response = await client.get("http://ytapi.stevesteacher.com/quotes")
     # we get the entire response, but we are primarily interested in the body
     var body = await response.body
     # we can then parse the body into pure JSON. We can then use .to to
     # convert the JSON into a type Nim can understand (optional)
     # without converting it, you will get a JsonNode type
-    return parseJson(body).to(seq[FactObj])
+    return parseJson(body).to(seq[QuoteObj])
 
 proc main() {.async.} =
     try:
         # here we just await fetching data from our API
-        var catFacts: seq[FactObj] = await fetchCatFacts()
-        echo catFacts[0].text
+        var quotes: seq[QuoteObj] = await fetchQuotes()
+        echo quotes[0]
         # if the request failed, use this except
     except:
-        echo "Failed to fetch cat facts."
+        echo "Failed to fetch quotes facts."
 
 # we need to use waitFor so we wait for the main() function
 # to finish executing before the program ends. waitFor is usually
